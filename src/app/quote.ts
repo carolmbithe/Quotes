e4f2a074b003f06926ac1893d7baebacec487b4d
carolmbithe <carombithe97@gmail.com>
2018-09-17 14:46:50 +0300

    added constructor properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,7 @@
 export class Quote {
-}
+
+    constructor(public id:number,public quote:string,public author:string,public person:string,public votes:number){}
+  }
+  
+  // ,public createdTime:Date
+  
