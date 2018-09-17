a24524540c5c160702ea5e9fd8a5f8131490705d
carolmbithe <carombithe97@gmail.com>
2018-09-17 16:04:59 +0300

    added a quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuotedetailsComponent } from './quotedetails/quotedetails.component';
+import { QuoteformComponent } from './quoteform/quoteform.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
-    QuotedetailsComponent
+    QuotedetailsComponent,
+    QuoteformComponent
   ],
   imports: [
     BrowserModule
